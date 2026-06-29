e5ac3535ffbd9d8c2d483594f8357a414290a81b
Hithaishi U <hithuurhc21@gmail.com>
2026-06-29 21:44:13 +0530

    Use Railway port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 require("./config/db");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const userRoutes = require("./routes/userRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
